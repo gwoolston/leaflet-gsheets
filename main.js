85bfe84dccc7016cd52dff3c74e7efadbd472400
gwoolston <gwoolston@me.com>
2024-02-21 12:24:08 -0500

    Update main.js
    update Sheets links

--- main.js
+++ main.js
@@ -9,9 +9,9 @@
 // these URLs come from Google Sheets 'shareable link' form
 // the first is the geometry layer and the second the points
 let geomURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vTsAyA0Hpk_-WpKyN1dfqi5IPEIC3rqEiL-uwElxJpw_U7BYntc8sDw-8sWsL87JCDU4lVg2aNi65ES/pub?output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vQOPeHbGxkf91D3OJMJIyVKkKeWTIntYIOO4quh17AGJBdMAHhdXada__LVEMNq_ZtFgjwLUwS18qIx/pub?output=csv";
 let pointsURL =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSFQw9sVY16eQmN5TIjOH7CUaxeZnl_v6LcdE2goig1pSe9I3hipeOn1sOwmC4fS0AURefRWwcKExct/pub?output=csv";
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSau6Ahj2C_c206-YM5Wp_faUseKzmM3uoxdmXGjUv_cGpWabeee5kyZaIxzxqAEtRNKbhIsYh4BJMJ/pub?output=csv";
 
 window.addEventListener("DOMContentLoaded", init);
 
