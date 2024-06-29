cff8c02aeb538b5747f54630e36a2cc66eb012e9
Gregory <gwoolston@me.com>
2024-06-29 16:44:10 -0400

    labels on top

--- main.js
+++ main.js
@@ -42,7 +42,9 @@ function init() {
   var labelsBasemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
   subdomains: 'abcd',
-  maxZoom: 16
+  maxZoom: 16,
+  pane: 'overlayPane',
+  zIndex: 500
 }).addTo(map);
 
   map.on("click", function () {
